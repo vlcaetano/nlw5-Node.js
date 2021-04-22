e3836f0044d2176e614348dcdb504b07f815ca17
Vitor Caetano <vitorlc95@gmail.com>
2021-04-22 16:56:33 -0300

    Day 2: Creating database

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express, { request, response } from 'express'
 
+import "./database"
+import { routes } from "./routes"
+
 const app = express()
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW-05!"
-  })
-})
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso"
-  })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
